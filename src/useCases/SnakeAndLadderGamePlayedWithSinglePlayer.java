3317fe8abd87d688107024123cf6b56f447b754a
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-08 09:37:38 +0530

    Added : Use Case 1 Problem - Snake and Ladder game played with single player at start position 0

---
+++ src/useCases/SnakeAndLadderGamePlayedWithSinglePlayer.java
@@ -0,0 +1,17 @@
+//This is a Snake and Ladder game simulation program where a single player is playing, starting from zero.
+
+package useCases;
+
+public class SnakeAndLadderGamePlayedWithSinglePlayer {
+    SnakeAndLadderGamePlayedWithSinglePlayer()
+    {
+
+        System.out.println("Welcome to Snake and Ladder Simulation \n");
+    }
+    public static void main(String[] args) {
+        SnakeAndLadderGamePlayedWithSinglePlayer a = new SnakeAndLadderGamePlayedWithSinglePlayer();
+        int startingPosition = 0;
+        System.out.println("Starting Position of the Single Player is : "+startingPosition);
+    }
+
+}
