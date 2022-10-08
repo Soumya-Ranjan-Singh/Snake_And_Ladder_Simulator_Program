aa0fc442e2c6ed9ede4aafbaaa465bdffa5a83fb
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-08 09:42:53 +0530

    Added : Use Case 2 Problem - The Player rolls the die to get a number between 1 to 6

---
+++ src/useCases/RollingTheDieToGetANumber.java
@@ -0,0 +1,25 @@
+/*This is a Snake and Ladder game simulation program where a single player is playing, starting from zero,
+and he is rolling the die to get a number.*/
+
+package useCases;
+
+import java.util.Random;
+
+public class RollingTheDieToGetANumber {
+    RollingTheDieToGetANumber()
+    {
+        System.out.println("Welcome to Snake and Ladder Simulation \n");
+    }
+
+    public static void main(String[] args) {
+        RollingTheDieToGetANumber a = new RollingTheDieToGetANumber();
+        Random random = new Random();
+        int startingPosition = 0;
+        System.out.println("Starting Position of the Single Player is : "+startingPosition+"\n");
+
+        //Rolling the die.
+        int dieRolls = (random.nextInt(6)+1);
+        System.out.println("The Player rolls the die and gets a number : "+dieRolls);
+    }
+
+}
